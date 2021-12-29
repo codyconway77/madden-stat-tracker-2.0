e5b712fdf128bcc648ada960d016a74d6c319c8c
codyconway77 <67038097+codyconway77@users.noreply.github.com>
2021-12-29 15:51:33 -0800

    Added component for team select in game input form and cleaned up names, ids, and labels for inputs

--- components/GameForm.tsx
+++ components/GameForm.tsx
@@ -1,4 +1,5 @@
 import React, { FormEventHandler } from 'react'
+import TeamSelect from './TeamSelect'
 
 function GameForm() {
     const handleSubmit: FormEventHandler = async (event) => {
@@ -17,46 +18,46 @@ function GameForm() {
         const result = await res.json()
     } 
     return (
-        <form onSubmit={handleSubmit} className='w-4/6 grid grid-cols-2 gap-3'>
+        <form onSubmit={handleSubmit} className='w-4/6 grid grid-cols-2 gap-5'>
             <div className='col-start-1 col-end-3 flex flex-col'>
                 <label htmlFor='opponent' className='text-center'>Opponent</label>
                 <input id='opponent' name='opponent' type='text' className='outline-1 outline rounded-md'></input>
             </div>
             <div className='flex flex-col col-start-1 col-end-2'>
                 <label htmlFor='team' className='text-center'>Team</label>
-                <input type='text' id='team' name='team' required autoComplete='off' className='outline-1 outline rounded-md'/>
+                <TeamSelect name="team" />
                 <label htmlFor='score' className='text-center'>Score</label>
                 <input type='number' id='score' name='score' required autoComplete='off' className='outline-1 outline rounded-md'/>
-                <label htmlFor='team' className='text-center'>Passing Yards</label>
-                <input type='text' id='team' name='team' required autoComplete='off' className='outline-1 outline rounded-md'/>
-                <label htmlFor='score' className='text-center'>Rushing Yards</label>
-                <input type='number' id='score' name='score' required autoComplete='off' className='outline-1 outline rounded-md'/>
-                <label htmlFor='team' className='text-center'>Passing TDs</label>
-                <input type='text' id='team' name='team' required autoComplete='off' className='outline-1 outline rounded-md'/>
-                <label htmlFor='score' className='text-center'>Rushing Tds</label>
-                <input type='number' id='score' name='score' required autoComplete='off' className='outline-1 outline rounded-md'/>
-                <label htmlFor='team' className='text-center'>Forced Fumbles</label>
-                <input type='text' id='team' name='team' required autoComplete='off' className='outline-1 outline rounded-md'/>
-                <label htmlFor='score' className='text-center'>Interceptions</label>
-                <input type='number' id='score' name='score' required autoComplete='off' className='outline-1 outline rounded-md'/>
+                <label htmlFor='passing yards' className='text-center'>Passing Yards</label>
+                <input type='number' id='passing yards' name='passing yards' required autoComplete='off' className='outline-1 outline rounded-md'/>
+                <label htmlFor='rushing yards' className='text-center'>Rushing Yards</label>
+                <input type='number' id='rushing yards' name='rushing yards' required autoComplete='off' className='outline-1 outline rounded-md'/>
+                <label htmlFor='passing tds' className='text-center'>Passing TDs</label>
+                <input type='number' id='passing tds' name='passing tds' required autoComplete='off' className='outline-1 outline rounded-md'/>
+                <label htmlFor='rushing tds' className='text-center'>Rushing Tds</label>
+                <input type='number' id='rushing tds' name='rushing tds' required autoComplete='off' className='outline-1 outline rounded-md'/>
+                <label htmlFor='forced fumbles' className='text-center'>Forced Fumbles</label>
+                <input type='number' id='forced fumbles' name='forced fumbles' required autoComplete='off' className='outline-1 outline rounded-md'/>
+                <label htmlFor='interceptions' className='text-center'>Interceptions</label>
+                <input type='number' id='interceptions' name='interceptions' required autoComplete='off' className='outline-1 outline rounded-md'/>
             </div>
             <div className='flex flex-col col-start-2 col-end-3'>
                 <label htmlFor='opposingTeam' className='text-center'>Opp Team</label>
-                <input type='text' id='opposingTeam' name='opposingTeam' required autoComplete='off' className='outline-1 outline rounded-md'/>
+                <TeamSelect name="opposingTeam" />
                 <label htmlFor='opponentScore' className='text-center'>Opp Score</label>
                 <input type='number' id='opponentScore' name='opponentScore' required autoComplete='off' className='outline-1 outline rounded-md'/>
-                <label htmlFor='opposingTeam' className='text-center'>Opp Pass Yards</label>
-                <input type='text' id='opposingTeam' name='opposingTeam' required autoComplete='off' className='outline-1 outline rounded-md'/>
-                <label htmlFor='opponentScore' className='text-center'>Opp Rush Yards</label>
-                <input type='number' id='opponentScore' name='opponentScore' required autoComplete='off' className='outline-1 outline rounded-md'/>
-                <label htmlFor='opposingTeam' className='text-center'>Opp Passing TDs</label>
-                <input type='text' id='opposingTeam' name='opposingTeam' required autoComplete='off' className='outline-1 outline rounded-md'/>
-                <label htmlFor='opponentScore' className='text-center'>Opp Rushing TDs</label>
-                <input type='number' id='opponentScore' name='opponentScore' required autoComplete='off' className='outline-1 outline rounded-md'/>
-                <label htmlFor='opposingTeam' className='text-center'>Opp FF</label>
-                <input type='text' id='opposingTeam' name='opposingTeam' required autoComplete='off' className='outline-1 outline rounded-md'/>
-                <label htmlFor='opponentScore' className='text-center'>Opp Interceptions</label>
-                <input type='number' id='opponentScore' name='opponentScore' required autoComplete='off' className='outline-1 outline rounded-md'/>
+                <label htmlFor='opponent passing yards' className='text-center'>Opp Pass Yards</label>
+                <input type='number' id='opponent passing yards' name='opponent passing yards' required autoComplete='off' className='outline-1 outline rounded-md'/>
+                <label htmlFor='opponent rushing yards' className='text-center'>Opp Rush Yards</label>
+                <input type='number' id='opponent rushing yards' name='opponent rushing yards' required autoComplete='off' className='outline-1 outline rounded-md'/>
+                <label htmlFor='opponent passing tds' className='text-center'>Opp Passing TDs</label>
+                <input type='number' id='opponent passing tds' name='opponent passing tds' required autoComplete='off' className='outline-1 outline rounded-md'/>
+                <label htmlFor='opponent rushing tds' className='text-center'>Opp Rushing TDs</label>
+                <input type='number' id='opponent rushing tds' name='opponent rushing tds' required autoComplete='off' className='outline-1 outline rounded-md'/>
+                <label htmlFor='opponent forced fumbles' className='text-center'>Opp FF</label>
+                <input type='number' id='opponent forced fumbles' name='opponent forced fumbles' required autoComplete='off' className='outline-1 outline rounded-md'/>
+                <label htmlFor='opponent interceptions' className='text-center'>Opp Interceptions</label>
+                <input type='number' id='opponent interceptions' name='opponent interceptions' required autoComplete='off' className='outline-1 outline rounded-md'/>
             </div>
             <button type='submit' className='col-start-1 col-end-3 outline outline-stone-700 rounded-md focus:bg-stone-500 focus:text-white shadow-md shadow-stone-700 hover:shadow-inner hover:shadow-stone-700  hover:text-stone-700'>Submit</button>
         </form>
---
+++ components/TeamSelect.tsx
@@ -0,0 +1,42 @@
+function TeamSelect(props: { name: string }) {
+    return (
+        <>
+            <select required name={props.name} id={props.name} className='outline-1 outline rounded-md'>
+                <option value="49ers">49ers</option>
+                <option value="Bears">Bears</option>
+                <option value="Bengals">Bengals</option>
+                <option value="Bills">Bills</option>
+                <option value="Broncos">Broncos</option>
+                <option value="Browns">Browns</option>
+                <option value="Bucanners">Bucaneers</option>
+                <option value="Cardinals">Cardinals</option>
+                <option value="Chargers">Chargers</option>
+                <option value="Cheifs">Cheifs</option>
+                <option value="Colts">Colts</option>
+                <option value="Cowboys">Cowboys</option>
+                <option value="Dolphins">Dolphins</option>
+                <option value="Eagles">Eagles</option>
+                <option value="Falcons">Falcons</option>
+                <option value="Football Team">Football Team</option>
+                <option value="Giants">Giants</option>
+                <option value="Jaguars">Jaguars</option>
+                <option value="Jets">Jets</option>
+                <option value="Lions">Lions</option>
+                <option value="Packers">Packers</option>
+                <option value="Panthers">Panthers</option>
+                <option value="Patriots">Patriots</option>
+                <option value="Raiders">Raiders</option>
+                <option value="Rams">Rams</option>
+                <option value="Ravens">Ravens</option>
+                <option value="Saints">Saints</option>
+                <option value="Seahawks">Seahawks</option>
+                <option value="Steelers">Steelers</option>
+                <option value="Texans">Texans</option>
+                <option value="Titans">Titans</option>
+                <option value="Vikings">Vikings</option>
+            </select>    
+        </>
+    )
+}
+
+export default TeamSelect
