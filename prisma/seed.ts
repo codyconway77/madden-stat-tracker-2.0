55e288db87a949fba92fd539bb8b5abfb4fccf46
Codyconway77 <codyconway77@gmail.com>
2022-01-05 11:34:37 -0800

    feat(dev): made a seed for database and created an instance of prismaClient in lib folder
    seed.ts runs on prisma migrate and primsa seed to fill database with dummy data for development purpose

---
+++ prisma/seed.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+
+async function main() {
+    
+    const bob = await prisma.user.upsert({
+      where: { userName: 'bob' },
+      update: {},
+      create: {
+        userName: 'bob',
+        hashedPassword: 'bob-3478-afeaerasdfewfa-ekjofpaore',
+      },
+    })
+
+    const raiders = await prisma.team.upsert({
+        where: { id: 1 },
+        update: {},
+        create: {
+            userId: 1
+        }
+    })
+
+    const game1 = await prisma.game.upsert({
+        where: { id: 1 },
+        update: {},
+        create: {
+            userId: 1,
+            teamId: 1,
+            opponent: 'Bob',
+            oppTeam: '49ers',
+            score: 27,
+            oppScore: 3,
+            passingYards: 268,
+            oppPassingYards: 148,
+            rushingYards: 87,
+            oppRushingYards: 29,
+            passingTds: 3,
+            oppPassingTds: 0,
+            rushingTds: 0,
+            oppRushingTds: 0,
+            interceptions: 3,
+            oppInterceptions: 1,
+            forcedFumbles: 0,
+            oppForcedFumbles: 1
+        },
+    })
+    
+    console.log({ bob, raiders, game1 })
+    }
+    
+    main()
+    .catch((e) => {
+        console.error(e)
+        process.exit(1)
+    })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
