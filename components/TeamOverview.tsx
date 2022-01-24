633811bf6a7668ac70cc3eae8e75197eb379f99b
Codyconway77 <codyconway77@gmail.com>
2022-01-23 22:18:04 -0800

    fix: add more stats to team overview component

--- components/TeamOverview.tsx
+++ components/TeamOverview.tsx
@@ -13,17 +13,30 @@ const gameScoreReducer = (game: Game) => game.score
 
 const gameWinsReducer = (game: Game) => game.win
 
+const gameRYardsReducer = (game: Game) => game.rushingYards
+const gamePYardsReducer = (game: Game) => game.passingYards
+
 const TeamOverview: React.FC<ITeamProps> = ({ team }) => {
     const totalWins = team.games.map(gameWinsReducer).filter(win => win).length
     const totalLosses = team.games.map(gameWinsReducer).filter(win => !win).length
     const totalTeamScore = team.games.map(gameScoreReducer).reduce((prev: number, next: number) => prev + next)
     const averageScore = totalTeamScore / team.games.length
+    const totalYards = team.games.map(gameRYardsReducer).reduce((prev: number, next: number) => prev + next) + team.games.map(gamePYardsReducer).reduce((prev: number, next: number) => prev + next)
+    const averageYards = totalYards / team.games.length
     return (
-        <div className='bg-stone-300 p-6 shadow-lg focus:shadow-inner hover:shadow-inner hover:shadow-stone-500 shadow-stone-500 rounded-2xl flex flex-col jusitfy-around items-center'>
+        <div className='bg-stone-300 p-6 shadow-lg focus:shadow-inner hover:shadow-inner hover:shadow-stone-500 shadow-stone-500 rounded-2xl flex flex-col gap-1 jusitfy-around items-center'>
             <h2 className='text-xl text-white p-2 rounded-lg font-extrabold bg-stone-500 my-1'>{team.name}</h2>
-            <p className='text-xl'>{totalWins} - {totalLosses}</p>
-            <p className='text-xl'>Total Score: {totalTeamScore}</p>
-            <p className='text-xl'>Average Score: {averageScore}</p>
+            <p className='text-xl font-bold'>{totalWins} - {totalLosses}</p>
+            <div className='w-full flex justify-around'>
+                <div className='flex flex-col mx-2 justify-around'>
+                    <p className='text-xl'>Total Score: {totalTeamScore}</p>
+                    <p className='text-xl'>Average Score: {averageScore}</p>
+                </div>
+                <div className='flex flex-col mx-2 justify-around'>
+                    <p className='text-xl'>Total Yards: {totalYards}</p>
+                    <p className='text-xl'>Average Yards: {averageYards}</p>
+                </div>
+            </div>
         </div>
     )
 }
